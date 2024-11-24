6c116ac1bc33e1b1e71b8d2075a2a3cdf42dfc72
Surapheal Belay <shbelay@gmail.com>
2024-11-24 06:42:04 -0600

    no significant changes. formatting changes.

--- locals.tf
+++ locals.tf
@@ -4,5 +4,16 @@ locals {
     name = "app-network"
     address_prefixes = ["10.0.0.0/16"]
   }
+
   subnet_address_prefix = ["10.0.1.0/24","10.0.2.0/24"]
+  subnets=[
+    {
+        name="websubnet"
+        address_prefixes=["10.0.1.0/24"]
+    },
+    {
+        name="appsubnet"
+        address_prefixes=["10.0.2.0/24"]
+    }
+  ]
 }
